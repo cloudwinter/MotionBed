e4a9991b144f32e0a4d1faa6adbffb9b2d2f0aa6
xiayundong <1032176134@qq.com>
2022-05-07 21:42:52 +0800

    1.0.2-3
    添加同步控制eventMessage

---
+++ app/src/main/java/com/sn/blackdianqi/bean/MessageEvent.java
@@ -0,0 +1,37 @@
+package com.sn.blackdianqi.bean;
+
+/**
+ * Created by xiayundong on 2022/5/6.
+ */
+public class MessageEvent {
+
+    private boolean tongbukzShow;
+
+    private boolean tongbukzSwitch;
+
+    public MessageEvent() {
+    }
+
+    public MessageEvent(boolean tongbukzShow, boolean tongbukzSwitch) {
+        this.tongbukzShow = tongbukzShow;
+        this.tongbukzSwitch = tongbukzSwitch;
+    }
+
+    public boolean isTongbukzShow() {
+        return tongbukzShow;
+    }
+
+    public void setTongbukzShow(boolean tongbukzShow) {
+        this.tongbukzShow = tongbukzShow;
+    }
+
+    public boolean isTongbukzSwitch() {
+        return tongbukzSwitch;
+    }
+
+    public void setTongbukzSwitch(boolean tongbukzSwitch) {
+        this.tongbukzSwitch = tongbukzSwitch;
+    }
+
+
+}
